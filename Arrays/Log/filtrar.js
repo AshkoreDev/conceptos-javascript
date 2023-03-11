ab64591649d839213865e4d61726e0dc777eba75
Aracely Alba <aracelyalba8@gmail.com>
2023-03-11 14:24:55 -0400

    Added challenges with array methods

---
+++ Arrays/Log/filtrar.js
@@ -0,0 +1,14 @@
+// FILTRAR VALORES ÚNICOS EN UN ARRAY.
+const numbers = [2, 4, 6, 8, 2, 2, 4, 6];
+
+let item = Array.from(new Set(numbers));;
+
+console.log(item);
+
+
+// FILTRAR NÚMEROS IMPARES Y MULTIPLICARLOS POR 2.
+const numbers1 = [1, 2, 3, 4, 5, 6];
+
+let item1 = numbers1.filter(n => n % 2 !== 0).map(n => n * 2);
+
+console.log(item1);
---
+++ Arrays/Log/ordenar.js
@@ -0,0 +1,4 @@
+
+// RECUPERAR ALEATORIAMENTE UN ELEMENTO DEL ARRAY.
+let item1 = Math.floor(Math.random() * numbers.length);
+console.log(numbers[item1]);
