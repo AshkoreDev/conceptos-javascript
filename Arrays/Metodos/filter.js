dbf5477d49b509371256eddc1dd683d1a2a7d29f
Aracely Alba <aracelyalba8@gmail.com>
2023-03-13 16:43:40 -0400

    Added Filter and Find, array methods

---
+++ Arrays/Metodos/filter.js
@@ -0,0 +1,9 @@
+// FILTER, se usa para filtrar elementos de un array que cumplan con una condición.
+// Los elementos que cumplan con la condición serán parte del nuevo array generado.
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const item = numbers.filter(item => item >= 5 && item <= 10);
+
+console.log(item);
---
+++ Arrays/Metodos/find.js
@@ -0,0 +1,21 @@
+// FIND, se usa para filtrar el primer elemento de un array que cumpla con una condición.
+
+
+const budgets = [
+	{
+		city: 'California',
+		budget: 5500
+	},
+	{
+		city: 'New York',
+		budget: 8000
+	},
+	{
+		city: 'Orlando',
+		budget: 2000
+	}
+];
+
+const item = budgets.find(item => item.budget < 8000);
+
+console.log(item);
